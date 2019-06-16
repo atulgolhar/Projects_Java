7c29fa2a685aaa89ef06cf30db5274fe19fe6e00
Atul Golhar <atulgolhar@gmail.com>
2019-06-16 17:54:07 -0400

    Create Practice.java

---
+++ Practice.java
@@ -0,0 +1,16 @@
+public class practice {
+	public static void main(String[] args) {
+		System.out.println("Assignment Operators");
+		int x = 10;
+		System.out.println(x);
+		x += 5;
+		System.out.println(x);
+		x -= 6;
+		System.out.println(x);
+		x *= 3;
+		System.out.println(x);
+		x /= 3;
+		System.out.println(x);
+	}
+	
+}
