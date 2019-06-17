621e9e6d5e806bd874b8152bbf033878c1e6a5c7
Atul Golhar <atulgolhar@gmail.com>
2019-06-17 12:11:06 -0400

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -1,16 +1,17 @@
+
 public class practice {
 	public static void main(String[] args) {
 		System.out.println("Assignment Operators");
-		int x = 10;
-		System.out.println(x);
-		x += 5;
-		System.out.println(x);
-		x -= 6;
-		System.out.println(x);
-		x *= 3;
-		System.out.println(x);
-		x /= 3;
-		System.out.println(x);
+		float x = 10; System.out.println(x);
+		x += 5;System.out.println("x += 5; " + x);
+		x /= 2; System.out.println("x /= 2; " + x);
+		x *= 3; System.out.println("*= 3; " + x);
+		x /= 4; System.out.print("/= 4; " + x + "");
+		System.out.println("; Need to limit decimals");
+		++x; System.out.println("++x; " + x);
+		++x; System.out.println("++x; " + x);
+		--x; System.out.println("--x; " + x);
+		x *= 4; System.out.println("*= 4; " + x);
 	}
 	
 }
